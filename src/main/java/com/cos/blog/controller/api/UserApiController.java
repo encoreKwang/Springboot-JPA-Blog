d0643596f468822ac4b549ab4491df0a9b4e3614
kwangyong <dnflrhkddyd@naver.com>
2023-01-18 03:02:10 +0900

    spring security start

--- src/main/java/com/cos/blog/controller/api/UserApiController.java
+++ src/main/java/com/cos/blog/controller/api/UserApiController.java
@@ -1,5 +1,7 @@
 package com.cos.blog.controller.api;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,7 +19,7 @@ public class UserApiController {
 	@Autowired
 	UserService userService;
 	
-	@PostMapping("/api/user")
+	@PostMapping("/auth/joinProc")
 	public ResponseDto<Integer> save(@RequestBody User user) {
 		System.out.println("UserApiController save 호출됨");
 		user.setRole(RoleType.USER);
@@ -25,4 +27,15 @@ public class UserApiController {
 		return new ResponseDto<Integer>(HttpStatus.OK.value(), 1);
 		//자바오브젝트를 Jackson라이브러리가 JSON데이터로 변환해서 리턴함.
 	}
+	
+	//전통적인 로그인 방식
+	/*
+	 * @PostMapping("/api/user/login") public ResponseDto<Integer>
+	 * login(@RequestBody User user, HttpSession session){
+	 * System.out.println("UserApiController login 호출됨"); User principal =
+	 * userService.로그인(user); //pricipal (접근 주체) if(principal != null) {
+	 * session.setAttribute("principal", principal); }
+	 * 
+	 * return new ResponseDto<Integer>(HttpStatus.OK.value(), 1); }
+	 */
 }
